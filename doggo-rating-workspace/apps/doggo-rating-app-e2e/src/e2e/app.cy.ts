21b0aac444f1ffbd7906503a8dd37e6e48da1486
RobinB87 <robinbette@hotmail.com>
2024-01-21 11:47:44 +0000

    add doggo base project

--- doggo-rating-workspace/apps/doggo-rating-app-e2e/src/e2e/app.cy.ts
+++ doggo-rating-workspace/apps/doggo-rating-app-e2e/src/e2e/app.cy.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('doggo-rating-app-e2e', () => {
+describe('doggo-rating-app', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('doggo-rating-app-e2e', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Welcome/);
+    getGreeting().contains('Welcome doggo-rating-app');
   });
 });
--- doggo-rating-workspace/apps/doggo-rating-app/src/app/app.component.ts
+++ doggo-rating-workspace/apps/doggo-rating-app/src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AuthActions } from './auth/store/auth.actions';
+import { LayoutComponent } from './shell/layout/layout.component';
 
 @Component({
-  standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'doggo-rating-workspace-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  standalone: true,
+  styleUrls: ['./app.component.css'],
+  imports: [LayoutComponent],
 })
-export class AppComponent {
-  title = 'doggo-rating-app';
+export class AppComponent implements OnInit {
+  title = 'ratemydoggo';
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.checkAuth();
+  }
+
+  private checkAuth() {
+    this.store.dispatch(AuthActions.checkAuth());
+  }
 }
